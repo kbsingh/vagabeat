528668054f58578c2c08cb3e07332e1c760e0bbc
Karanbir Singh <kbsingh@karan.org>
2015-05-11 21:51:44 +0100

    run a yum update if its needed

--- vagrant_test.sh
+++ vagrant_test.sh
@@ -5,15 +5,14 @@ vagrant init atomicapp/dev
 vagrant up
 UpdtPkgs=$(vagrant ssh -c "sudo yum -d0 list updates | wc -l")
 echo 'Updates backlog :' ${UpdtPkgs} 
-#if [ $UpdtPkgs -gt 10 ]; then
-#    echo 'More than 10 packages due an update!'
-#    exit 1
-#else
-    vagrant ssh -c 'cd sync; sudo env "PATH=$PATH" bash test_example_helloapache.sh'
-    if [ $? -ne 0 ]; then
-      echo 'failed'
-      exit 2
-    else
-      exit 0
-    fi
-#fi
+if [ $UpdtPkgs -gt 0 ]; then
+    echo 'Box needs an update'
+    vagrant ssh -c "sudo yum -y update"
+fi
+vagrant ssh -c 'cd sync; sudo env "PATH=$PATH" bash test_example_helloapache.sh'
+if [ $? -ne 0 ]; then
+  echo 'failed'
+  exit 2
+else
+  exit 0
+fi
