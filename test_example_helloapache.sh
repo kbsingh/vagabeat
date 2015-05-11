e2fb1ed7b06b4b06e0138ac3ef25c2b06a689d7f
Karanbir Singh <kbsingh@karan.org>
2015-05-11 21:49:01 +0100

    Just watch for the app to be up and running; put in a max timeout

--- test_example_helloapache.sh
+++ test_example_helloapache.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
+
 atomic install aweiteka/helloapache:app
+if [ $? -ne 0 ]; then echo "atomic install failed" ; exit 1 ; fi
+
 atomic run aweiteka/helloapache:app
-sleep 2
-kubectl get pod | grep -q fedora
+if [ $? -ne 0 ]; then echo "atomic run failed" ; exit 1 ; fi
+
+#waiting for pod to come up
+podup=$(kubectl get pod | grep -E 'apache.*Running' ; echo $?)
+timer=0
+echo 'Waiting for upto 4 minutes for the app to launch...'
+while [ $timer -lt 120 ] && [ $podup -gt 0 ]; do
+  echo -n '+'
+  sleep 2
+  podup=$(kubectl get pod | grep -E 'apache.*Running' ; echo $?)
+  timer=$((timer+1))
+done
+echo ' '
+if [ $podup -gt 0 ]; then
+  echo 'Gave up waiting for app to start'
+  exit 1
+fi
+
+# This is really our assert...
+curl http://localhost/ | grep -q Apache
 if [ $? -eq 0 ]; then
-  stat=$(kubectrl get pod | grep fedora | awk '{print $7}')
-  while [ $stat != 'Running' ]; do
-    sleep 2
-    stat=$(kubectrl get pod | grep fedora | awk '{print $7}')
-  done
-  ret=$(curl http://localhost/)
-  if [ $ret == 'Apache' ]; then
-    exit 0
-  else 
-    echo 'Returned value now what we expected'
-    exit 1
-  fi
+  echo "Sucess!"
   exit 0
 else
-  echo 'Kube ctrl failed to launch pod'
-  exit 1
+ echo 'Returned value now what we expected'
+ exit 1
 fi
