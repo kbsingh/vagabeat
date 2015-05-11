879a0e75e59fd9710a5f1fdb85a191a56f63aeb3
Karanbir Singh <kbsingh@karan.org>
2015-05-12 00:20:00 +0100

    skip updating the vagrant box

--- vagrant_test.sh
+++ vagrant_test.sh
@@ -5,10 +5,10 @@ vagrant init atomicapp/dev
 vagrant up
 UpdtPkgs=$(vagrant ssh -c "sudo yum -d0 list updates | wc -l")
 echo 'Updates backlog :' ${UpdtPkgs} 
-if [ $UpdtPkgs -gt 0 ]; then
-    echo 'Box needs an update'
-    vagrant ssh -c "sudo yum -y update"
-fi
+#if [ $UpdtPkgs -gt 0 ]; then
+#    echo 'Box needs an update'
+#    vagrant ssh -c "sudo yum -y update"
+#fi
 vagrant ssh -c 'cd sync; sudo env "PATH=$PATH" bash test_example_helloapache.sh' -- -t
 if [ $? -ne 0 ]; then
   echo 'failed'
